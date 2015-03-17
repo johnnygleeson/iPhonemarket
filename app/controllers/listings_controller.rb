c21a400470dcb2ba9a61d366d728abc3c6386921
John <your.johnnygleeson@gmail.com>
2015-03-17 20:32:22 +0000

    sign up fix

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -31,7 +31,17 @@ class ListingsController < ApplicationController
   def create
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+      token = params[:stripeToken]
+      recipient = Stripe::Recipient.create(
+        :name => curent_user.name,
+        :type => "individual",
+        :bank_account => token
+        )
 
+    current_user.recipient = recipient.id
+    current_user.save
+    
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316173819) do
+ActiveRecord::Schema.define(version: 20150317163202) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150316173819) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
